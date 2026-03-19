96627fb97705900f9c66f831e46fffd8ea2b7d29
DavidB <davidvenga@yahoo.com>
2026-03-19 17:55:38 -0400

    Add clothes fixed

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -2,12 +2,14 @@ import "./Header.css";
 import logo from "../../assets/Logo-wtwr.svg";
 import avatar from "../../assets/avatar-wtwr.svg";
 
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <header className="header">
       <img className="header__logo" src={logo} />
       <p className="header__date-location">Date-Location</p>
-      <button className="header__add-clothes-btn">+ Add Clothes</button>
+      <button className="header__add-clothes-btn" onClick={handleAddClick}>
+        + Add Clothes
+      </button>
       <div className="header__user-container">
         <p className="header__username">Name</p>
         <img src={avatar} alt="Name" className="header__avatar" />
