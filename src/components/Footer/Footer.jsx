924fc4ca1b6e0d5a1690796d550cddb0cf6eddcb
DavidB <davidvenga@yahoo.com>
2026-03-20 19:40:09 -0400

    footer troubleshooting

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__name">Developed by David Baron</p>
+      <p className="footer__year">2026</p>
+    </footer>
+  );
+}
+
+export default Footer;
